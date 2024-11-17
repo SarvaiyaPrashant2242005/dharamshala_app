a57ff2d503800e0b0c8c4b941cabf52e3d59ab95
neelraiyani5 <125970410+neelraiyani5@users.noreply.github.com>
2024-11-17 16:43:09 +0530

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dharamshala_app/firebase_options.dart';
 import 'package:dharamshala_app/login/login_screen.dart';
+import 'package:dharamshala_app/splacescreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: LoginSignUpScreen(),
+      home: SplashScreen(),
     );
   }
 }
